8db66b1949db5caca09773e966a56a00dcc979a1
Arunendra Singh <singharunendra978@gmail.com>
2025-01-01 22:57:50 +0530

    Basic setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen and do what you can do⬆️⬆️😕😕🤣🤣😍😍.</Text>
     </View>
   );
 }
