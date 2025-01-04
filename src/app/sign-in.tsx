061226d545d1226b39001386b3342487666f0201
Arunendra Singh <singharunendra978@gmail.com>
2025-01-04 09:47:00 +0530

    adds assets and ng

--- src/app/sign-in.tsx
+++ src/app/sign-in.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const SignIn = () => {
   return (
     <View>
+      <View>
+        <Image />
+      </View>
       <Text>This is a signIn Page</Text>
     </View>
   )
---
+++ src/constant/images.ts
@@ -0,0 +1,1 @@
+import onboarding from '@/src/assets/images/onboarding.jpg'
